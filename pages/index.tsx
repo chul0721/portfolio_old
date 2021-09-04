165594e5bf182c41e93c9a4efc68dd72bf544ef1
chul0721 <chul070721@naver.com>
2021-09-05 00:22:18 +0900

    style: code format

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,13 @@ class Home extends React.Component<UserAgentProps> {
 
     return (
       <>
-        {(ua?.isIE || ua?.isSafari) ? <BrowserSupport /> : <></>}
+        {ua?.isIE || ua?.isSafari ? <BrowserSupport /> : <></>}
 
         <section className="w-screen h-screen">
-          <div className="test flex justify-center text-6xl font-bold pt-56" data-aos="fade-down">
-          </div>
+          <div
+            className="test flex justify-center text-6xl font-bold pt-56"
+            data-aos="fade-down"
+          ></div>
           <span className="flex justify-center text-2xl font-semibold" data-aos="fade-right">
             Byung Chul Kim
           </span>
